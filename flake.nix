cfdfb7a78233f2b5d1ba7744f3eac526110a2d24
mzntori <mzntori@proton.me>
2026-05-14 13:04:24 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "custom status bar";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      packages."x86_64-linux".default = pkgs.rustPlatform.buildRustPackage rec {
+        pname = "aether-status";
+        version = "0.1.0";
+        cargoLock.lockFile = ./Cargo.lock;
+        src = pkgs.lib.cleanSource ./.;
+      };
+    };
+}
